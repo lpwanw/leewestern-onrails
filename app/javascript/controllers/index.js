7a732be4171716111c0cc331de5602fce5f8d667
lpwanw <phuongtay2000@gmail.com>
2024-08-25 11:17:15 +0700

    fix: remove unsuse stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,9 +4,6 @@
 
 import { application } from "./application"
 
-import DisplayEditorController from "./display_editor_controller"
-application.register("display-editor", DisplayEditorController)
-
 import DropdownController from "./dropdown_controller"
 application.register("dropdown", DropdownController)
 
