242aa29df25ab3a4f60d6ef5fba1d6e5036b1990
lpwanw <phuongtay2000@gmail.com>
2024-08-25 15:01:26 +0700

    Quote

--- app/components/editor/quote/component.rb
+++ app/components/editor/quote/component.rb
@@ -6,4 +6,23 @@ class Editor::Quote::Component < Editor::BaseComponent
   def tag_name
     "blockquote"
   end
+
+  def options
+    {
+      class: class_names,
+    }
+  end
+
+  def class_names
+    alignment_class = "text-center" if alignment == "center"
+    [alignment_class]
+  end
+
+  def alignment
+    @alignment ||= block.dig(:data, :alignment)
+  end
+
+  def caption
+    @caption ||= block.dig(:data, :caption)
+  end
 end
