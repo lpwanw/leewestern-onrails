d62ddfc7b68ba323adb2509e655100a3b3187b83
lpwanw <phuongtay2000@gmail.com>
2024-09-06 16:29:27 +0700

    Update Factory Post

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -5,6 +5,10 @@ FactoryBot.define do
     user
     title { FFaker::Lorem.sentence }
     shared_post { nil }
+    content do
+      body = "<h1>#{FFaker::Lorem.sentence}</h1><div>#{FFaker::Lorem.paragraph}</div>"
+      ActionText::RichText.new(body:)
+    end
     post_type { "post" }
     status { "published" }
   end
