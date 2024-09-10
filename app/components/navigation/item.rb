f78a2d72decd84704c2395cf94a4eddadb7198c9
lpwanw <phuongtay2000@gmail.com>
2024-09-10 20:58:57 +0700

    Navigation Active

--- app/components/navigation/item.rb
+++ app/components/navigation/item.rb
@@ -9,18 +9,27 @@ class Navigation::Item < ViewComponent::Base
     user: "icon/user",
   }.freeze
 
-  def initialize(icon, html_options = {})
+  ACTIVE_ICONS = {
+    home: "icon/home_active",
+    add: "icon/add",
+    more: "icon/more",
+    sign_in: "icon/sign_in",
+    user: "icon/user",
+  }.freeze
+
+  def initialize(icon, html_options = {}, active: false)
     @icon = icon
     @html_options = html_options
+    @active = active
     super
   end
 
   private
 
-  attr_reader :icon, :html_options
+  attr_reader :icon, :html_options, :active
 
   def icon_svg
-    ICONS[icon]
+    @active ? ACTIVE_ICONS[icon] : ICONS[icon]
   end
 
   def class_options
