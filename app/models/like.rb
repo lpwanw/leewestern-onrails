1e27b7e62a2283f18aacd761716a4b72c74a8f0e
lpwanw <phuongtay2000@gmail.com>
2024-09-18 21:53:59 +0700

    Update notification behavior

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,13 +4,16 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :likeable, polymorphic: true, counter_cache: true
 
-  has_one :notification, as: :source, dependent: :destroy
+  # rubocop:disable Rails/HasManyOrHasOneDependent
+  has_one :notification, as: :source
+  # rubocop:enable Rails/HasManyOrHasOneDependent
 
   validates :user_id, uniqueness: { scope: %i[likeable_id likeable_type] }
 
   delegate :notification_user, to: :likeable
 
   after_create_commit :trigger_notification
+  before_destroy :revert_notification_source
 
   def trigger_notification
     return if user.id == likeable.user_id
@@ -28,5 +31,12 @@ class Like < ApplicationRecord
         target_user: likeable.user,
       )
     end
+  end
+
+  def revert_notification_source
+    prev_like = likeable.likes.where.not(id:).order(id: :desc).first
+    return notification.destroy unless prev_like
+
+    notification.update(source: prev_like)
   end
 end
