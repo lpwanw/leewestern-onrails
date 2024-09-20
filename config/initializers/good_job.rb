bfb43c45deba5e0c3f7b9120f14511a6d00590b6
lpwanw <phuongtay2000@gmail.com>
2024-09-20 12:53:51 +0700

    Keep me alive on Render

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -3,6 +3,7 @@
 Rails.application.configure do
   config.good_job.enable_cron = true
   config.good_job.cleanup_preserved_jobs_before_seconds_ago = 1.hour
+  config.good_job.execution_mode = :async
 
   save_my_self = if ENV.fetch("ON_RENDER", false)
                    {
