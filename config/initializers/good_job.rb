d0ce5b093ad93a75b52c90d30f23a55798bfd854
lpwanw <phuongtay2000@gmail.com>
2024-09-25 14:46:05 +0700

    remove cron

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -4,18 +4,4 @@ Rails.application.configure do
   config.good_job.enable_cron = true
   config.good_job.cleanup_preserved_jobs_before_seconds_ago = 1.hour
   config.good_job.execution_mode = :async
-
-  save_my_self = if ENV.fetch("ON_RENDER", false)
-                   {
-                     cron: "*/1 * * * *",
-                     class: "KeepMeAliveJob",
-                     description: "Keep the service running on Render",
-                   }
-                 else
-                   {}
-                 end
-
-  config.good_job.cron = {
-    save_my_self:,
-  }
 end
