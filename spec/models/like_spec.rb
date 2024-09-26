6c98aa1477c777c3cfa8b2a8dedebe2a2e1728e9
lpwanw <phuongtay2000@gmail.com>
2024-09-26 22:04:07 +0700

    Rspec Like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -13,4 +13,29 @@ RSpec.describe Like do
 
     it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(%i[likeable_id likeable_type]) }
   end
+
+  describe "#trigger_notification" do
+    let(:likeable) { create(:post) }
+    let(:like) { build(:like, likeable:) }
+
+    before { allow(like).to receive(:trigger_notification).and_call_original }
+
+    it do
+      like.save
+      expect(like).to have_received(:trigger_notification)
+    end
+
+    it { expect { like.save }.to change(Notification, :count).by(1)}
+  end
+
+  describe "#revert_notification_source" do
+    let!(:likeable) { create(:post) }
+    let!(:prev_like) { create(:like, likeable:) }
+    let!(:like) { create(:like, likeable:) }
+    let!(:notification) { like.notification }
+
+    before { like.destroy }
+
+    it { expect(notification.reload.source).to eq prev_like }
+  end
 end
