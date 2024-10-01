f3a886cc65e63876e5e1e6ecb51eb10a40c87534
lpwanw <phuongtay2000@gmail.com>
2024-10-01 18:07:53 +0700

    fix: action cable adafter

--- config/initializers/sentry.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Sentry.init do |config|
-  config.breadcrumbs_logger = %i[active_support_logger http_logger]
-  config.dsn = ENV.fetch("SENTRY_DSN", nil)
-  config.enable_tracing = true
-end
